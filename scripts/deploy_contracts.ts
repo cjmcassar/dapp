2b16ec91b7b4f9bd85f728e674b098ee9cf8edb0
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-04 17:31:10 -0400

    updated deploy contract

---
+++ scripts/deploy_contracts.ts
@@ -0,0 +1,11 @@
+import { ethers } from "ethers";
+import { Ballot__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function main() {}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
