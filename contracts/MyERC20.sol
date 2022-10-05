77ff579707017f9924192cff6dc447f1320c7bfa
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-05 18:42:23 -0400

    Added burnable function

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyERC2Token is ERC20, AccessControl {
+contract MyERC20Token is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("MyERC2Token", "MTK") {
+    constructor() ERC20("MyERC20Token", "MTK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
