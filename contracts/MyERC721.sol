5fc7fdc62d6234b4afe9a100cdc0bc86da0e4b44
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-05 18:42:40 -0400

    Updated naming convention

--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyToken is ERC721 {
+
+contract MyERC721Token is ERC721 {
     constructor() ERC721("MyToken", "MTK") {}
 }
