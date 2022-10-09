bb0dd6043145a947ed39b4fd1792878eeec2ad7c
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-09 17:49:20 -0400

    Updated with options const

--- scripts/DeployToken.ts
+++ scripts/DeployToken.ts
@@ -1,12 +1,19 @@
-import { PromiseOrValue } from "../typechain-types/common";
 import { ethers } from "ethers";
 import { MyToken__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-	const provider = ethers.getDefaultProvider("goerli");
-	const wallet = ethers.Wallet.createRandom();
+	const options = {
+		alchemy: process.env.ALCHEMY_API_KEY,
+		infura: process.env.INFURA_API_KEY,
+	};
+
+	const provider = ethers.getDefaultProvider("goerli", options);
+	const wallet = new ethers.Wallet(
+		process.env.PRIVATE_KEY ??
+			"683def552ac24f83434628e742eb722aa99ac67484fb543904f6c8229251f9a2"
+	);
 	const signer = wallet.connect(provider);
 
 	const tokenContractFactory = new MyToken__factory(signer);
