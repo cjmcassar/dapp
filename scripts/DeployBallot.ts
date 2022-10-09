e27f137857128203b746db50cdcd02b68711c324
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-09 18:27:53 -0400

    removed info

--- scripts/DeployBallot.ts
+++ scripts/DeployBallot.ts
@@ -1,16 +1,24 @@
-import { PromiseOrValue } from "../typechain-types/common";
 import { ethers } from "ethers";
 import { MyToken__factory, TokenizedBallot__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
-const TOKEN_CONTRACT = "token contract address";
+const PROPOSALS = [
+	"0x50726f706f73616c000000000000000000000000000000000000000000000000",
+	"0x50726f706f73616c000000000000000000000000000000000000000000000000",
+	"0x50726f706f73616c000000000000000000000000000000000000000000000000",
+];
+const TOKEN_CONTRACT = "0x107052260BfC157cab653c6C01BADbDD1A4Bd424";
 const VOTING_POWER = 1;
 
 async function main() {
-	const provider = ethers.getDefaultProvider("goerli");
-	const wallet = ethers.Wallet.createRandom();
+	const options = {
+		alchemy: process.env.ALCHEMY_API_KEY,
+		infura: process.env.INFURA_API_KEY,
+	};
+
+	const provider = ethers.getDefaultProvider("goerli", options);
+	const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
 	const signer = wallet.connect(provider);
 
 	const tokenBallotContractFactory = new TokenizedBallot__factory(signer);
